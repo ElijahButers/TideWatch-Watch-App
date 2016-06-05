a0e0eb9fdbb003d01b1ee79604a40a81f72bda03
ElijahButers <Elijahbuters@gmail.com>
2016-06-05 21:12:27 +0300

    Added ComplicationController class to the TideWatch WatchKit App Extension target and set as the Data Source Class in complication configuration.

---
+++ TideWatch WatchKit App Extension/ComplicationController.swift
@@ -0,0 +1,13 @@
+//
+//  ComplicationController.swift
+//  TideWatch
+//
+//  Created by User on 6/5/16.
+//  Copyright © 2016 razeware. All rights reserved.
+//
+
+import UIKit
+
+class ComplicationController: NSObject {
+
+}
